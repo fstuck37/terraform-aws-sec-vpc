2dddf117950ffc76670cda387139b3e02e31f3d7
Stuck <StuckF@dnb.com>
2021-06-25 15:01:01 -0400

    initial work

--- txgw.tf
+++ txgw.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_ec2_transit_gateway_vpc_attachment" "txgw_attachment" {
   count                   = var.transit_gateway_id == false ? 0 : 1
   subnet_ids              = local.map_subnet_id_list[element(var.subnet-order,1)]
@@ -6,7 +7,10 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "txgw_attachment" {
   appliance_mode_support  = var.appliance_mode_support 
 }
 
-/*
+
+
+
+
 resource "aws_route" "txgw-routes" {
   for_each               = {for item in local.txgw_routes : item.name => item}
   route_table_id         = each.value.route_table
