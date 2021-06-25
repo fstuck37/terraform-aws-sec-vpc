a2b369bca6c5ec9230d53d89f6c59838740d7d22
Stuck <StuckF@dnb.com>
2021-06-25 15:22:09 -0400

    initial work

--- natgw.tf
+++ natgw.tf
@@ -1,8 +1,11 @@
 /* NOTE: Hard coded to ngw layer */
 resource "aws_nat_gateway" "natgw" {
   for_each = {for i in local.subnet_data:i.name=>i
-           if contains(i, "ngw")
+           if replace(i, "ngw", "") != i
   }
   allocation_id  = aws_eip.eip[each.value.az].id
   subnet_id      = aws_subnet.subnets[each.value.name].id
 }
+
+
+  
