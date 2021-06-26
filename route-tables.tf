9c9a182111e122cf040a22dd84caac0ce2ad8782
Stuck <StuckF@dnb.com>
2021-06-26 07:44:07 -0400

    initial work

--- route-tables.tf
+++ route-tables.tf
@@ -17,7 +17,7 @@ resource "aws_route_table_association" "associations" {
 
 resource "aws_route" "ngw-default-route" {
   for_each = {for sd in local.subnet_data:sd.name=>sd
-           if sd.az == "ngw" }
+           if sd.layer == "ngw" }
   route_table_id         = aws_route_table.routers[each.value.name].id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_egress_only_internet_gateway.eg-inet-gw.id
