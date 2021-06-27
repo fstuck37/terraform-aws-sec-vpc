c62c142aa39c574655220e65129eaf1ac33c5c08
Stuck <StuckF@dnb.com>
2021-06-27 15:49:57 -0400

    initialize

--- route-tables.tf
+++ route-tables.tf
@@ -33,11 +33,11 @@ resource "aws_route" "txgw-routes" {
 
 
 resource "aws_route" "txgw-routes-ep" {
-  for_each = {for i, sd in local.subnet_data:sd.name=>sd
+  for_each = {for sd in local.subnet_data:sd.name=>sd
            if sd.layer == "tgw" &&  var.aws_vpc_endpoint_id != [] }
   route_table_id         = aws_route_table.routers[each.value.name].id
   destination_cidr_block = "0.0.0.0/0"
-  vpc_endpoint_id        = var.aws_vpc_endpoint_id[i]
+  vpc_endpoint_id        = var.aws_vpc_endpoint_id[each.value.subnet_index]
 }
 
 
