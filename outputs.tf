42f4eed7cfdc10013579c04a4634b3bfd0612ceb
Stuck <StuckF@dnb.com>
2021-06-27 16:11:04 -0400

    initialize

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,11 @@ output "subnet_cidrs" {
   value = local.subnet_cidrs
 }
 
+output "subnet_layer_cidrs" {
+  description = "Map with keys based on the subnet names and values of subnet IDs"
+  value = local.subnet_cidrs
+}
+
 output "routetable_ids" {
   description = "Map with keys based on the subnet names and values of subnet IDs"
   value = local.routetable_ids
