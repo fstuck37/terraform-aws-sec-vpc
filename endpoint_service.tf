e27b7a6572ecf5541ca9dcef679c14a4334e8864
Stuck <StuckF@dnb.com>
2021-07-07 17:48:10 -0400

    update

--- endpoint_service.tf
+++ endpoint_service.tf
@@ -2,14 +2,18 @@ resource "aws_vpc_endpoint_service" "gateway" {
   acceptance_required        = false
   gateway_load_balancer_arns = [aws_lb.gwlb.arn]
 
+  allowed_principals = var.endpoint_service_allowed_principal
+
   tags                 = merge(
     var.tags,
     map("Name",format("%s", var.vpc-name == true ? "${var.name-vars["account"]}-${replace(var.region,"-", "")}-${var.name-vars["name"]}" : var.vpc-name))
   )
 }
 
+/*
 resource "aws_vpc_endpoint_service_allowed_principal" "gateway" {
   for_each = toset(var.endpoint_service_allowed_principal)
     vpc_endpoint_service_id = aws_vpc_endpoint_service.gateway.id
     principal_arn           = each.value
 }
+*/
