dd4e4184e79d07ae9735e2c761591f21c9784afc
Stuck <StuckF@dnb.com>
2021-07-07 17:34:08 -0400

    gwlb

---
+++ gwlb.tf
@@ -0,0 +1,40 @@
+resource "aws_lb" "gwlb" {
+  name               = format("%s", var.vpc-name == true ? "${var.name-vars["account"]}-${replace(var.region,"-", "")}-${var.name-vars["name"]}" : var.vpc-name)
+  internal           = true
+  load_balancer_type = "gateway"
+
+  subnets            = local.subnet_ids["fwt"]
+  enable_deletion_protection = false
+
+/*
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.bucket
+    prefix  = "test-lb"
+    enabled = true
+  }
+*/
+
+  enable_cross_zone_load_balancing = true
+
+  tags = merge(
+    var.tags,
+    map("Name",format("%s", var.vpc-name == true ? "${var.name-vars["account"]}-${replace(var.region,"-", "")}-${var.name-vars["name"]}" : var.vpc-name)),
+    local.resource-tags["aws_lb"]
+  )
+}
+
+resource "aws_lb_target_group" "gwlbtg" {
+  name        = format("%s", var.vpc-name == true ? "${var.name-vars["account"]}-${replace(var.region,"-", "")}-${var.name-vars["name"]}" : var.vpc-name)
+  port        = 6081
+  protocol    = "GENEVE"
+  target_type = "instance"
+  vpc_id      = aws_vpc.main_vpc.id
+  
+  health_check {
+    enabled = true
+    port = 80
+    protocol = "tcp"
+    timeout = 5
+    unhealthy_threshold = 3
+  }
+}
--- variables.tf
+++ variables.tf
@@ -42,7 +42,7 @@ variable "tags" {
 
 variable "resource-tags" {
   type = map(map(string))
-  description = "Optional : A map of maps of tags to assign to specifc resources.  The key must be one of the following: aws_vpc, aws_vpn_gateway, aws_subnet, aws_network_acl, aws_internet_gateway, aws_cloudwatch_log_group, aws_vpc_dhcp_options, aws_route_table."
+  description = "Optional : A map of maps of tags to assign to specifc resources.  The key must be one of the following: aws_vpc, aws_vpn_gateway, aws_subnet, aws_network_acl, aws_internet_gateway, aws_cloudwatch_log_group, aws_vpc_dhcp_options, aws_route_table, aws_lb."
   default = { }
 }
 
